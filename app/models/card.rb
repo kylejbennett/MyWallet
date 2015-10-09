f6e56f0da5c61d7c80719f51ecdb927f79f5e5e0
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-10-09 16:26:00 -0400

    Added UserCards and credit card number and year validations using regex

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,9 +3,10 @@ class Card < ActiveRecord::Base
 	belongs_to :user
 	has_many :usercards, foreign_key: :card_id, dependent: :destroy
 
-	validates :card_number, length: {is: 16}
 	validates :card_type, presence: true
 	validates :exp_month, :numericality => { :less_than_or_equal_to => 12 } 	
-	validates :exp_year, length: {is: 4}
-	
+	validates :exp_year, length: {is: 4}, :numericality => { :greater_than_or_equal_to => 2015 }
+	validates_format_of :card_number, :with => /(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(3[47][0-9]{13})/
+	validates_format_of :exp_year, :with => /20[1-9]{2}/
+
 end
