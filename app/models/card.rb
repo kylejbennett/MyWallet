38b3481fd201e08f9d1ed1c032e49032c2cece14
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-11-14 17:06:46 -0500

    fixed issue with not allowing expiration year of 2020

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,6 +7,6 @@ class Card < ActiveRecord::Base
 	validates :exp_month, :numericality => { :less_than_or_equal_to => 12 } 	
 	validates :exp_year, length: {is: 4}, :numericality => { :greater_than_or_equal_to => 2015 }
 	validates_format_of :card_number, :with => /(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(3[47][0-9]{13})/
-	validates_format_of :exp_year, :with => /20[1-9]{2}/
+	validates_format_of :exp_year, :with => /20[1-9][0-9]/
 
 end
